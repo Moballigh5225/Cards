c1b232f7582b350270faf149560bf5d1c7c73624
Moballighul islam <89847264+Moballigh5225@users.noreply.github.com>
2025-07-26 11:25:51 +0530

    final commit

--- main.go
+++ main.go
@@ -1,37 +1,24 @@
 package main
 
-import "fmt"
-
 
 
 
 
 func main(){
+	cards :=  newDeck()
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+
+	cards.toString()
 	
-	oddEven := []int{0,1,2,3,4,5,6,7,8,9,10}
+	cards.saveToFile("myCards")
+	(newDeckFromFile("my")).print()
 
-	for _, number := range oddEven{
-		if number % 2 == 0 {
-			fmt.Println(number, "is even")
-		}else{
-			fmt.Println(number, "is odd")
-		}
-	}
-
-	// cards :=  newDeck()
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
-
-	// cards.toString()
-	
-	// cards.saveToFile("myCards")
-	// (newDeckFromFile("my")).print()
-
-	// cards.shuffle()
-	// cards.print()
+	cards.shuffle()
+	cards.print()
 }
 
-type oddEven []int
+
 
